ca85bdada19b6e0bf3e21e23d47e43c863f5db50
Racheal-spec <folorunshotosin334@gmail.com>
2023-07-18 03:19:41 -0300

    implemented signin using cookie, jwt and auth

--- app/api/registerApi.ts
+++ app/api/registerApi.ts
@@ -6,25 +6,27 @@ import { serialize } from "cookie";
 export async function registerApi(req: NextApiRequest, res: NextApiResponse) {
   try {
     //get prisma to create a user
-    const user = await db.user.create({
-      data: {
-        anonname: req.body.anonname,
-        uniqueid: req.body.uniqueid,
-        password: await hashPassword(req.body.password),
-      },
-    });
-    const jwt = await createJWT(user);
-    //Why cookies instead of local storage? Using cookies here because (i) it reduces the work on the clientside (ii) The nextjs middleware that I'm going to be using has no access to local storage becuase it happens outside of the computer/server i.e on the edge
-    res.setHeader(
-      "Set-Cookie",
-      //Below is the cookie i'M Setting
-      serialize(process.env.COOKIE_NAME as string, jwt, {
-        httpOnly: true,
-        path: "/",
-        maxAge: 60 * 6 * 24 * 7,
-      })
-    );
-    console.log(user);
+    if (req.method === "POST") {
+      const user = await db.user.create({
+        data: {
+          anonname: req.body.anonname,
+          uniqueid: req.body.uniqueid,
+          password: await hashPassword(req.body.password),
+        },
+      });
+      const jwt = await createJWT(user);
+      //Why cookies instead of local storage? Using cookies here because (i) it reduces the work on the clientside (ii) The nextjs middleware that I'm going to be using has no access to local storage becuase it happens outside of the computer/server i.e on the edge
+      res.setHeader(
+        "Set-Cookie",
+        //Below is the cookie i'M Setting
+        serialize(process.env.COOKIE_NAME as string, jwt, {
+          httpOnly: true,
+          path: "/",
+          maxAge: 60 * 6 * 24 * 7,
+        })
+      );
+      console.log(user);
+    }
     return res.status(201);
   } catch (error) {
     return res.status(500).json(error);
